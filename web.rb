408db5497d7df818164bce36f9b0cde0c9a3619d
CurmudJim <jgarvey86@gmail.com>
2018-01-08 18:52:28 -0600

    Added contact form

--- web.rb
+++ web.rb
@@ -4,6 +4,27 @@ require 'pony'
 require './themoviedb.rb'
 
 
+def send_message
+
+    Pony.mail({
+      :from => params[:name] + "<" + params[:email] + ">",
+      :to => 'jgarvey86@gmail.com',
+      :subject => params[:name] + " has contacted you",
+      :body => params[:message],
+      :via => :smtp,
+      :via_options => {
+        :address              => 'smtp.gmail.com',
+        :port                 => '587',
+        :enable_starttls_auto => true,
+        :user_name            => 'jgarvey86',
+        :password             => 'rwlnzgeahrbmzbtz',
+        :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+        :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+      }
+    })
+
+end
+
 
 get '/' do
   @movies = ["Enter a movie."]
@@ -19,4 +40,13 @@ post '/' do
   @posters = @movies[4]
 
   erb :index
+end
+
+get '/contact' do
+  erb :contact
+end
+
+post '/contact' do
+  send_message
+  redirect to('/')
 end
